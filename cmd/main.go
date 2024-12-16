ef05bbf2367d268e5671a1e1032be8aa2b6c1f60
neandrson <a2361@yandex.ru>
2024-12-16 21:40:32 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+type Config struct {
+	Port string `json: "port"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Привет, мир!"))
+}
+
 func main() {
-	//mux := http.NewServeMux()
-
-	// Создаём обработчик для маршрута "/api/v1/calculate"
-	//hello := http.HandlerFunc(helloHandler)
-
-	// Применяем logging middleware к обработчику "/api/v1/calculate"
-	http.Handle("/api/v1/calculate", http.HandlerFunc())
-
-	// Запускаем сервер на порту 8080
+	http.HandleFunc("/calculate", calculateHandler)
+	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
