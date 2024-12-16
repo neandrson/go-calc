28845495927dec89f2bc16c3dea7b8689715ce19
neandrson <a2361@yandex.ru>
2024-12-16 16:56:16 +0300

    Create calculation.go

---
+++ pkg/calculation/calculation.go
@@ -0,0 +1,114 @@
+package application
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var res, cal, sum int
+
+func Calc(expression string) (float64, error) {
+
+	expression = strings.ReplaceAll(expression, " ", "")
+	if !isValid(expression) {
+		return 0, fmt.Errorf("Invalid expression")
+	}
+	postfix := infixToPostfix(expression)
+	result, err := evaluatePostfix(postfix)
+	if err != nil {
+		return 0, err
+	}
+	return result, nil
+}
+
+func isValid(expression string) bool {
+
+	validChars := "0123456789+-*/()"
+	for _, char := range expression {
+		if !strings.Contains(validChars, string(char)) {
+			return false
+		}
+	}
+	openCount := strings.Count(expression, "(")
+	closeCount := strings.Count(expression, ")")
+	if openCount != closeCount {
+		return false
+	}
+	return true
+}
+
+func infixToPostfix(expression string) []string {
+	var postfix []string
+	var stack []string
+
+	precedence := map[string]int{
+		"+": 1,
+		"-": 1,
+		"*": 2,
+		"/": 2,
+	}
+
+	tokens := strings.Split(expression, "")
+
+	for _, token := range tokens {
+		if token == "(" {
+			stack = append(stack, token)
+		} else if token == ")" {
+			for stack[len(stack)-1] != "(" {
+				postfix = append(postfix, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+		} else if _, isOperator := precedence[token]; isOperator {
+			for len(stack) > 0 && precedence[stack[len(stack)-1]] >= precedence[token] {
+				postfix = append(postfix, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, token)
+		} else {
+			postfix = append(postfix, token)
+		}
+	}
+
+	for len(stack) > 0 {
+		postfix = append(postfix, stack[len(stack)-1])
+		stack = stack[:len(stack)-1]
+	}
+
+	return postfix
+}
+
+func evaluatePostfix(postfix []string) (float64, error) {
+	var stack []float64
+
+	for _, token := range postfix {
+		if num, err := strconv.ParseFloat(token, 64); err == nil {
+			stack = append(stack, num)
+		} else {
+			if len(stack) < 2 {
+				return 0, fmt.Errorf("Invalid expression")
+			}
+
+			num2 := stack[len(stack)-1]
+			num1 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+
+			switch token {
+			case "+":
+				stack = append(stack, num1+num2)
+			case "-":
+				stack = append(stack, num1-num2)
+			case "*":
+				stack = append(stack, num1*num2)
+			case "/":
+				stack = append(stack, num1/num2)
+			}
+		}
+	}
+	if len(stack) != 1 {
+		return 0, fmt.Errorf("Invalid expression")
+	}
+
+	return stack[0], nil
+}
