3fc2b6207382fe5bd610a4b752c9ddb762403065
neandrson <a2361@yandex.ru>
2024-12-17 16:30:02 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
+type response struct {
+	Name string `json:"name"`
+}
+
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
+	result := response{}
+	result.Name = r.URL.Query().Get("name")
+	jsonOut, _ := json.Marshal(result)
+	log.Print(string([]byte(jsonOut)))
+	json.NewEncoder(w).Encode(jsonOut)
 	w.Write([]byte("Привет, мир!"))
 }
 
 func main() {
-	http.HandleFunc("/api/v1/calculate", calculateHandler)
-	fmt.Println("Server is running on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	apiURL := "https://localhost/api/v1/calculate"
+
+	resp, err := http.Post(apiURL, "application/json", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.StatusCode)
+    fmt.Println(resp)
+	defer resp.Body.Close()
 }
