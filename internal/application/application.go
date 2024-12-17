0867896285a4b491aec89e19782a82ab58f231a1
neandrson <a2361@yandex.ru>
2024-12-17 22:37:49 +0300

    Update application.go

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,24 +1,82 @@
 package application
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-func calculateHandler(w http.ResponseWriter, r *http.Request) {
-	var req Request
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+type Config struct {
+	Addr string
+}
+
+type Application struct {
+	config *Config
+}
+
+type Request struct {
+	Expression string `json: "expression"`
+}
+
+func New() *Application {
+	return &Application{
+		config: ConfigFromEnv(),
+	}
+}
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	request := new(Request)
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	result, err := calculate(req.Expression)
+	result, err := calculation.Calc(request.Expression)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(Response{Error: err.Error()})
-		return
+		fmt.Fprintf(w, "err^ %s", err.Error())
+	} else {
+		fmt.Fprintf(w, "result: %f", result)
+	}
+}
+
+func ConfigFromEnv() *Config {
+	config := new(Config)
+	config.Addr = os.Getenv("PORT")
+	if config.Addr == "" {
+		config.Addr = "8080"
+	}
+	return config
+}
+
+func (a Application) Run() {
+	log.Println("input expression")
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString("\n")
+	if err != nil {
+		log.Println("filed ti read expression from console")
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Response{Result: result})
+	text = strings.TrimSpace(text)
+	if text == "exit" {
+		log.Println("application was successfully closed")
+		return nil
+	}
+
+	result, err := calculation.Calc(text)
+	if err != nil {
+		fmt.Println(text, "calculation failed wit error", err)
+	} else {
+		fmt.Println(text, "=", result)
+	}
+}
+
+func (a *Application) RunServer() error {
+	http.HandleFunc("/", CalcHandler)
+	return http.ListenAndServe(":"+a.config.Addr, nil)
 }
