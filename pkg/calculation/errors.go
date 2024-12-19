a6193bf9477d513b147d4643444d6829ee244417
neandrson <a2361@yandex.ru>
2024-12-19 16:10:16 +0300

    Update errors.go

--- pkg/calculation/errors.go
+++ pkg/calculation/errors.go
@@ -3,6 +3,7 @@ package calculation
 import "errors"
 
 var (
-	ErrInvalidExpression = errors.New("invalid expression")
+	ErrInvalidExpression = errors.New("Expression is not valid")
 	ErrDivisionByZero    = errors.New("division by zero")
+	ErrInternalServer    = errors.New("Internal server error")
 )
