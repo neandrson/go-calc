459cad2c447ccf1fc62d3f345ec157c11fa47828
neandrson <a2361@yandex.ru>
2024-12-21 21:06:34 +0300

    Update calculation.go

--- pkg/calculation/calculation.go
+++ pkg/calculation/calculation.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-//var res, cal, sum int
+const ErrExpressionValid = "Expression is not valid"
 
 /*func stringToFloat64(str string) float64 {
 	degree := float64(1)
@@ -30,7 +30,7 @@ func Calc(expression string) (float64, error) {
 
 	expression = strings.ReplaceAll(expression, " ", "")
 	if !isValid(expression) {
-		return 0, fmt.Errorf("expression is not valid")
+		return 0, fmt.Errorf("%s", ErrExpressionValid)
 	}
 	postfix := infixToPostfix(expression)
 	result, err := evaluatePostfix(postfix)
@@ -105,7 +105,7 @@ func evaluatePostfix(postfix []string) (float64, error) {
 			stack = append(stack, num)
 		} else {
 			if len(stack) < 2 {
-				return 0, fmt.Errorf("expression is not valid")
+				return 0, fmt.Errorf(ErrExpressionValid)
 			}
 
 			num2 := stack[len(stack)-1]
@@ -121,14 +121,14 @@ func evaluatePostfix(postfix []string) (float64, error) {
 				stack = append(stack, num1*num2)
 			case "/":
 				if num2 == 0 {
-					return 0, fmt.Errorf("expression is not valid")
+					return 0, fmt.Errorf(ErrExpressionValid)
 				}
 				stack = append(stack, num1/num2)
 			}
 		}
 	}
 	if len(stack) != 1 {
-		return 0, fmt.Errorf("expression is not valid")
+		return 0, fmt.Errorf(ErrExpressionValid)
 	}
 
 	return stack[0], nil
