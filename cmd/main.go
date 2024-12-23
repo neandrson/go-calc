0969c4bd3f8998d276331dbb2c4b06be5eba05f3
neandrson <a2361@yandex.ru>
2024-12-23 12:17:18 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/neandrson/go-calc/internal/application"
 )
 
 func main() {
 	app := application.New()
-	//app.Run()
-	app.RunServer()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/v1/calculate", application.CalcHandler)
+
+	log.Println("Starting server on port", app.Config.Addr)
+	if err := http.ListenAndServe(":"+app.Config.Addr, mux); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
